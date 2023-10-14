5b2fbded7f69805d305b4017eefa5aceb05a311e
Cloudtastrophe <ifiwil@yahoo.com>
2023-10-13 18:16:09 -0700

    0x04 Project commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function checks for a digit (0 through)
+ * @a: first integer
+ * @b: second integer
+ * Return : Result of multiplication
+ */
+
+int mul(int a, int b)
+{
+return a * b ; 
+}

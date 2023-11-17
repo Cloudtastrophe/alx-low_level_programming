aaa7b9b690a94fb80d5ecc57a1f511a6b95ff79b
Cloudtastrophe <ifiwil@yahoo.com>
2023-11-17 01:27:37 -0800

    Another commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <_putchar.c>
+#ifndef LINKED 
+#define LINKED
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
